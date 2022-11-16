8ac117e15b15c47270de21f579bdbe8528178e6e
Camila Echeverría <cecheverriac@uc.cl>
2022-11-16 10:21:37 -0300

    factores exponenciales for the win

--- 2.resultados/me-dio-la-misma-wea-ayuda.R
+++ 2.resultados/me-dio-la-misma-wea-ayuda.R
@@ -18,7 +18,7 @@ base1 <- base %>%
 
 base1 <- base1[-1,]
 
-# Cantidades totales ------------------------------------------------------
+# Cantidades totales zona ------------------------------------------------------
 
 viviendas.totales <- sum(as.numeric(base1[,5]))
 
@@ -28,7 +28,7 @@ viviendas.zona <- list(z1 = sum(as.numeric(base1[which(base1$zona == 1),5])),
                        z4 = sum(as.numeric(base1[which(base1$zona == 4),5])))
 
 
-# Cantidades muestrales ---------------------------------------------------
+# Cantidades muestrales zona ---------------------------------------------------
 
 viviendas.muestrales.z <- list(z1 = sum(distinct(datos[which(datos$zona==1),], 
                                              IdMz, .keep_all = TRUE)$Nviv),
@@ -62,7 +62,7 @@ select.vivienda <- list(z1 = viviendas.manzana[[1]]/viviendas.muestrales.z[[1]],
                         z3 = viviendas.manzana[[3]]/viviendas.muestrales.z[[3]],
                         z4 = viviendas.manzana[[4]]/viviendas.muestrales.z[[4]])
 
-# survey ------------------------------------------------------------------
+# Factores según zona ------------------------------------------------------------------
 
 fexp1 <- select.manzana[[1]]^(-1)/select.vivienda[[1]]
 fexp2 <- select.manzana[[2]]^(-1)/select.vivienda[[2]]
@@ -70,3 +70,53 @@ fexp3 <- select.manzana[[3]]^(-1)/select.vivienda[[3]]
 fexp4 <- select.manzana[[4]]^(-1)/select.vivienda[[4]]
 
 
+# Cantidades totales si tiene tula o no -----------------------------------
+
+sexo_pob
+
+
+# Cantidades muestrales si tiene tula o no --------------------------------
+
+persona.mu <- list(tula = count(datos[which(datos$sexo == 1),], sexo)[,2],
+                   notula = count(datos[which(datos$sexo == 2),], sexo)[,2])
+
+
+# Probabilidades si tiene tula o no ---------------------------------------
+
+select.persona <- list(tula = persona.mu[[1]]/sexo_pob[1,2],
+                       notula = persona.mu[[2]]/sexo_pob[2,2])
+
+
+# Factores de expansión según tula ----------------------------------------
+
+fexpt <- select.persona[[1]]^(-1)
+fexpnt <- select.persona[[2]]^(-1)
+
+
+# Cantidades totales edad -------------------------------------------------
+
+cat.edad_pob
+
+
+# Cantidades muestrales edad ----------------------------------------------
+
+cat.edadmuestral <- list(j = count(datos[which(datos$cat.edad == 1),], cat.edad)[,2],
+                         ntj = count(datos[which(datos$cat.edad == 2),], cat.edad)[,2],
+                         ntv = count(datos[which(datos$cat.edad == 3),], cat.edad)[,2],
+                         v = count(datos[which(datos$cat.edad == 4),], cat.edad)[,2])
+
+
+# Probabilidad edad -------------------------------------------------------
+
+select.edad <- list(j = cat.edadmuestral[[1]]/cat.edad_pob[1,2],
+                    ntj = cat.edadmuestral[[2]]/cat.edad_pob[2,2],
+                    ntv = cat.edadmuestral[[3]]/cat.edad_pob[3,2],
+                    v = cat.edadmuestral[[4]]/cat.edad_pob[4,2])
+
+
+# Factores de expansión según edad ----------------------------------------
+
+fexpj <- 1/select.edad[[1]]
+fexpntj <- 1/select.edad[[2]]
+fexpntv <- 1/select.edad[[3]]
+fexpv <- 1/select.edad[[4]]
